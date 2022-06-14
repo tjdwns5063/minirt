2425f3b63b0e3b708c057ea9544ecf946e5518af
tjdwns5063@naver.com <tjdwns5063@naver.com>
2022-06-14 12:34:15 +0900

    불필요한 주석 삭제

--- srcs/io/parse2.c
+++ srcs/io/parse2.c
@@ -7,14 +7,8 @@ int	parse_sphere(t_data *data, char **tokens, int id)
 	sph = lst_new(id);
 	if (!sph)
 		return (0);
-	// sph->coord[X] = ft_atof(tokens[1]);
-	// sph->coord[Y] = ft_atof(tokens[2]);
-	// sph->coord[Z] = ft_atof(tokens[3]);
 	sph->point = init_vec(ft_atof(tokens[1]), ft_atof(tokens[2]), ft_atof(tokens[3]));
 	sph->diameter = ft_atof(tokens[4]);
-	// sph->rgb[R] = ft_atof(tokens[5]);
-	// sph->rgb[G] = ft_atof(tokens[6]);
-	// sph->rgb[B] = ft_atof(tokens[7]);
 	sph->rgb = init_vec(ft_atof(tokens[5]), ft_atof(tokens[6]), ft_atof(tokens[7]));
 	lst_addback(&(data->objs), sph);
 	return (1);
@@ -27,17 +21,8 @@ int parse_plane(t_data *data, char **tokens, int id)
 	pl = lst_new(id);
 	if (!pl)
 		return (0);
-	// pl->coord[X] = ft_atof(tokens[1]);
-	// pl->coord[Y] = ft_atof(tokens[2]);
-	// pl->coord[Z] = ft_atof(tokens[3]);
 	pl->point = init_vec(ft_atof(tokens[1]), ft_atof(tokens[2]), ft_atof(tokens[3]));
-	// pl->norm_vec[X] = ft_atof(tokens[4]);
-	// pl->norm_vec[Y] = ft_atof(tokens[5]);
-	// pl->norm_vec[Z] = ft_atof(tokens[6]);
 	pl->vec = init_vec(ft_atof(tokens[4]), ft_atof(tokens[5]), ft_atof(tokens[6]));
-	// pl->rgb[R] = ft_atof(tokens[7]);
-	// pl->rgb[G] = ft_atof(tokens[8]);
-	// pl->rgb[B] = ft_atof(tokens[9]);
 	pl->rgb = init_vec(ft_atof(tokens[7]), ft_atof(tokens[8]), ft_atof(tokens[9]));
 	lst_addback(&(data->objs), pl);
 	return (1);
--- srcs/io/read.c
+++ srcs/io/read.c
@@ -41,7 +41,6 @@ int	rt_read(char *filename, t_data *data)
 	if (!parse(fd, data))
 		return (0);
 	close(fd);
-	// check_range
 	if (!check_data(data))
 		return (0);
 	return (1);
